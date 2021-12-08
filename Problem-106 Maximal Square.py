659ee9a377a5dee3085f1e3807ce722148777ad2
Adhish Shrivastava <adhish70@gmail.com>
2021-12-08 13:22:07 -0500

    Initial Commit

---
+++ Problem-106 Maximal Square.py
@@ -0,0 +1,11 @@
+# 221. Maximal Square
+# https://leetcode.com/problems/maximal-square/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity:
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        pass
---
+++ Problem-107 Partition Array for Maximum Sum.py
@@ -0,0 +1,11 @@
+# 1043. Partition Array for Maximum Sum
+# https://leetcode.com/problems/partition-array-for-maximum-sum/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity:
+
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        pass
